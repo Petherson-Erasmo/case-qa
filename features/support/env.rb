a9a4c34d0ea82a9714bea9996cbd80b3f67e7259
Petherson Erasmo <petherson_erasmo@hotmail.com>
2021-03-18 13:37:28 -0300

    Ajustes para funcionar no Local
    Na pasta webdriver substituí o arquivo chromedriver para a versão do meu navegador que atualmente é a versão 89.0. No arquivo Gemfile, adicionei uma biblioteca chamada RSpec, para me ajudar a fazer as validações no site. No meu terminal estava falando que no arquivo env.rb o comando 'Selenium::WebDriver::Chrome.driver_path =' estava DEPRECATION e pedia para usar o comando 'Selenium::WebDriver::Chrome::Service.driver_path ='.

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,6 @@ require 'selenium-webdriver'
 require 'pry'
 
 Capybara.default_driver = :selenium_chrome
-Selenium::WebDriver::Chrome.driver_path = Dir['webdriver/*'].first
+Selenium::WebDriver::Chrome::Service.driver_path = Dir['webdriver/*'].first
 
 Capybara.default_max_wait_time = 10
